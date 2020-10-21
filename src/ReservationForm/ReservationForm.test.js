574148281ac71ac4dd8a0892b1acd327759ad969
Orlando Murcio <ato.murcio@gmail.com>
2020-10-21 11:12:31 -0600

    Unit test for what should the Form shoudl render

---
+++ src/ReservationForm/ReservationForm.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { ReservationForm } from './ReservationForm'
+import '@testing-library/jest-dom';
+describe('ReservationForm', () =>{
+    beforeEach(() => {
+
+    })
+
+    it('should be able to display the reservation form on the document', () => {
+        render(
+            <ReservationForm />
+        )
+        // screen.debug()
+        const nameInput = screen.getByPlaceholderText('guest name');
+        const dateInput = screen.getByPlaceholderText('desired date');
+        const timeInput = screen.getByPlaceholderText('desired time');
+        const guestCountInput = screen.getByPlaceholderText('desired guest count');
+        const makeReservationButton = screen.getByRole('button', { name: /make reservation/i });
+
+        expect(nameInput).toBeInTheDocument();
+        expect(dateInput).toBeInTheDocument();
+        expect(timeInput).toBeInTheDocument();
+        expect(guestCountInput).toBeInTheDocument();
+        expect(makeReservationButton).toBeInTheDocument();
+    })
+})
