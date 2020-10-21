5d0f72911a5dc3f9b643e2e2b4e348a8452785e8
Orlando Murcio <ato.murcio@gmail.com>
2020-10-21 09:44:39 -0600

    Style Reservations container

---
+++ src/Reservations/Reservations.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Reservation } from '../Reservation/Reservation'
+import './Reservations.css'
+
+export const  Reservations = () =>{
+    
+    return (
+        <div className='reservations-container'>
+            
+        </div>
+    )
+}
