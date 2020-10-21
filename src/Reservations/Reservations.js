fed779f44b14005d616fd1b189327b688e4696d0
Orlando Murcio <ato.murcio@gmail.com>
2020-10-21 10:08:54 -0600

    Pass prop from app parent

--- src/Reservations/Reservations.js
+++ src/Reservations/Reservations.js
@@ -3,10 +3,19 @@ import { Reservation } from '../Reservation/Reservation'
 import './Reservations.css'
 
 export const  Reservations = () =>{
-    
+
     return (
         <div className='reservations-container'>
-            
+            <Reservation />
         </div>
     )
 }
+
+
+/*
+date: "12/29"
+id: 1
+name: "Christie"
+number: 12
+time: "7:00"
+*/
