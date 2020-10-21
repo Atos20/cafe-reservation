2220cc2a9d61a7a883c6febe6eaf025315f671d4
Orlando Murcio <ato.murcio@gmail.com>
2020-10-21 12:12:04 -0600

    Add method to post a new reservation

--- src/ApiCalls/ApiCalls.js
+++ src/ApiCalls/ApiCalls.js
@@ -9,6 +9,26 @@ const ApiCalls = {
           .then(response => response.json())
           .then(promise => promise)
           .catch(error => console.log('error', error));
+    },
+
+    newREservations(newReservation){
+        const myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+
+        // const raw = JSON.stringify({"name":"Orlando","date":"2020-12-20","time":"10:00","number":10});
+        const reservation = JSON.stringify({newReservation);
+
+        const requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: reservation,
+        redirect: 'follow'
+        };
+
+        return fetch("http://localhost:3001/api/v1/reservations", requestOptions)
+        .then(response => response.json())
+        .then(promise => promise)
+        .catch(error => console.log('error', error));
     }
 }
 
