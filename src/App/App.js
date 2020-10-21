347b53c4faca7b464da554bdaf5d86e890b0e380
Orlando Murcio <ato.murcio@gmail.com>
2020-10-21 10:56:23 -0600

    Pass make new reservation function down to reservation form

--- src/App/App.js
+++ src/App/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import ApiCalls from '../ApiCalls/ApiCalls'
 import { Reservations } from '../Reservations/Reservations'
+import { ReservationForm } from '../ReservationForm/ReservationForm'
 class App extends Component {
   constructor(){
     super()
@@ -21,16 +22,27 @@ class App extends Component {
       this.setState({ error: `You've got a ${error.status} Error` })
     }
   }
+
+  makeNewReservation = (newReservation) => {
+    console.log(newReservation)
+    this.setState( {reservations : [...this.state.reservations,  newReservation] } )
+  }
   
   render() {
     const { reservations }= this.state
     return (
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
-        <div className='resy-form'>
 
-        </div>
-        <Reservations reservations={reservations}/>
+        <div className='resy-form'>
+        <ReservationForm 
+          makeNewReservation={this.makeNewReservation} 
+        />
+        </div>  
+
+        <Reservations 
+          reservations={reservations}
+        />
       </div>
     )
   }
